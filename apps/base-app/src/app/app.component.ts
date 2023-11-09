ce65eaf0af8fa1a434c8cb23d53a954e350e64de
Martijn van der Meij <martijn.vandermeij@vattenfall.nl>
2023-11-09 10:32:55 +0100

    feat: add tailwind to components

--- apps/base-app/src/app/app.component.ts
+++ apps/base-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { UsingTailwindModule } from '@tailwind-demo/using-tailwind';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, UsingTailwindModule],
   selector: 'tailwind-lib-demo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
